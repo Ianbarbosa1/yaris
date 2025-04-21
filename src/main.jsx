b19c5b6ea8c2ad074b1df0a405b6960fd931b02f
Ianbarbosa1 <iancosta.tec@gmail.com>
2025-04-21 20:35:38 -0300

    teste de alteração de locais

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './css/index.css'
-import './css/animacoes.css'
-import './css/responsivo.css'
 import App from './index.jsx'
 
 createRoot(document.getElementById('root')).render(
