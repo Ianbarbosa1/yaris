aa146356f1164f27e1558ed54080b07b6c7a9d6b
Ianbarbosa1 <iancosta.tec@gmail.com>
2025-04-22 21:33:00 -0300

    atualizações e testes

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './css/index.css'
+import './css/responsivo.css'
+import './css/animacoes.css'
 import App from './index.jsx'
 
 createRoot(document.getElementById('root')).render(
